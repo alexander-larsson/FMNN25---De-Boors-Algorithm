35a931d23539d87a201974df72d806982b7f337f
Alexander Larsson <alexx.1992@gmail.com>
2015-09-18 12:20:48 +0200

    Added missing self and a small function

--- Spline.py
+++ Spline.py
@@ -17,7 +17,7 @@ class Spline:
         """Computes s(u)"""
         # Belov is the steps from 1.9 in slides
         # 1. Find hot interval
-        i = (knots > u).argmax() # hot interval is [ui-1,ui]
+        i = (self.knots > self.u).argmax() # hot interval is [ui-1,ui]
 
         # 2. Select control points dI-3 ... dI (slides 1.9)
         # Slides say dI-2 ... dI+1 in many other places so I
@@ -26,9 +26,18 @@ class Spline:
         # + 1 over what it is on all the slides and python
         # slicing excludes the last index.
         # i is indexed from 0 in the slides.
-        ctrl_pts = d[i-3:i+1]
+        ctrl_pts = self.d[i-3:i+1]
 
         # 3. Run the blossom recursion
+
+    def __alpha__(self,u,ur,ul):
+        """
+        Parameters:
+        u = The u for wich we are evaluating s(u)
+        ur = The rightmost knot
+        ul = The leftmost knot
+        """
+        return (ur-u)/(ur-ul)
 
 
     def plot(self):
